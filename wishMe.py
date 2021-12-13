7052188b607489f897c22e206e70c09cc5a99665
Earth <meherab0608@gmail.com>
2021-12-13 17:57:11 +0600

    make Classes

--- wishMe.py
+++ wishMe.py
@@ -4,6 +4,7 @@ from pickle import dump,load
 from sys import exit
 
 
+
 class welcomeWindow():
     def __init__(self,windowhight,windowWidth,welcomeText) -> None:
         self.windowHight = windowhight
@@ -17,51 +18,58 @@ class welcomeWindow():
         mainText = Label(root,text=self.welcomeText,fg="white",bg="black",font=("Ubuntu-Medium.ttf",45))
         mainText.pack(fill="both",expand=1)
         root.mainloop()
-        storeLastDate()
+       
 
 
 
-def storeLastDate():
+class Main():
+    def __init__(self) -> None:
+        lastWishdata = self.loadLastDayData()
+        today = datetime.now().strftime(r'%d-%m-%Y').split('-')
+        timegap = self.getTimegap(today,lastWishdata)
 
-    date = datetime.now().strftime(r'%Y,%m,%d').split(",")
-    dateObj = date
-    f = open("data.pkl",'wb')
+        if timegap == 0:
+            welcomeWindow1 = welcomeWindow(600,400,"welcome back sir :)")
+            self.storeLastDate()
+            exit()
+        else:
+            self.storeLastDate()
+            exit()
 
-    dump(dateObj,f)
-
-
-def loadLastDayData():
-    try:
-        f = open("data.pkl","rb")
-        data = load(f)
-        return data
-    except Exception as e:
-        return None
-
-
-def getTimegap(today=list,lastDate=list) -> list:
-    lastDay = date(int(lastDate[0]),int(lastDate[1]),int(lastDate[2]))
-    # lastDay = date(2000,5,5)
-    today = date(int(today[2]),int(today[1]),int(today[0]))
-    return (today-lastDay).days
         
+    def storeLastDate(self):
+
+        date = datetime.now().strftime(r'%Y,%m,%d').split(",")
+        dateObj = date
+        f = open("data.pkl",'wb')
+
+        dump(dateObj,f)
+        print(dateObj)
+        f.close()
+
+    def loadLastDayData(self):
+        try:
+            f = open("data.pkl","rb")
+            data = load(f)
+            return data
+        except Exception as e:
+            self.storeLastDate()
+            exit()
+            
+
+    def getTimegap(self,today=list,lastDate=list) -> list:
+        lastDay = date(int(lastDate[0]),int(lastDate[1]),int(lastDate[2]))
+        today = date(int(today[2]),int(today[1]),int(today[0]))
+        return (today-lastDay).days
+            
 
 
-def wishMe():
 
-    lastWishdata = loadLastDayData()
-    today = datetime.now().strftime(r'%d-%m-%Y').split('-')
-    timegap = getTimegap(today,lastWishdata)
 
-    if timegap > 4:
-        welcomeWindow1 = welcomeWindow(600,400,"welcome back sir :)")
-        exit()
-    else:
-        storeLastDate()
-        exit()
 
 
 
 if __name__ =="__main__":
 
-    wishMe()
+    # window = welcomeWindow(600,300,"Welcome back sir")
+    main = Main()
